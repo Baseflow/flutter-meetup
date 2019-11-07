880de6ac12710814c314d5e70d031ba4414b66f1
Maurits van Beusekom <maurits@vnbskm.nl>
2019-11-07 16:27:51 +0100

    Added implementation of the BlocProvider and Bloc base class

---
+++ bloc_counter/start/lib/src/bloc_provider.dart
@@ -0,0 +1,37 @@
+import 'package:bloc_counter/src/bloc.dart';
+import 'package:flutter/widgets.dart';
+
+class BlocProvider<T extends Bloc> extends StatefulWidget {
+  BlocProvider({
+    Key key,
+    @required this.child,
+    @required this.bloc,
+  }): super(key: key);
+
+  final T bloc;
+  final Widget child;
+
+  @override
+  _BlocProviderState<T> createState() => _BlocProviderState<T>();
+
+  static T of<T extends Bloc>(BuildContext context){
+    final type = _typeOf<BlocProvider<T>>();
+    BlocProvider<T> provider = context.ancestorWidgetOfExactType(type);
+    return provider.bloc;
+  }
+
+  static Type _typeOf<T>() => T;
+}
+
+class _BlocProviderState<T> extends State<BlocProvider<Bloc>>{
+  @override
+  void dispose(){
+    widget.bloc.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return widget.child;
+  }
+}
